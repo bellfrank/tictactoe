9adc2e2125810ca8e1679ff2172ea33f37815350
bellfrank <franciscocw0101@gmail.com>
2022-05-31 12:19:21 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from django.shortcuts import redirect
+from flask import Flask, render_template, session, redirect, url_for
+# flask sessions to give everyone a different game
+from flask_session import Session
+from tempfile import mkdtemp
+
+app = Flask(__name__)
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    #store our board in a session
+    if "board" not in session:
+        session["board"] = [[None, None, None],[None, None, None], [None, None, None]]
+        session["turn"] = "X"
+    
+    return render_template("game.html", game=session["board"], turn=session["turn"])
+
+
+
+@app.route("/play/<int:row>/<int:col>")
+def play(row, col):
+    
+    return redirect(url_for("index"))
