65a190eed2e4897b7e12d40fe1bcacd92936d4c2
bellfrank <franciscocw0101@gmail.com>
2022-06-16 16:59:22 -0700

    flask issue with tic tac toe

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ from helpers import apology
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
