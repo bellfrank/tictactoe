412306a36f280fb6333ea87a6f8785304dd82304
bellfrank <franciscocw0101@gmail.com>
2022-06-16 17:02:40 -0700

    flask issue with tic tac toe

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# This creates an issue with my heroku application
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
